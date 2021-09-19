449dbf4805dd9d2f4ab82a650ddca80e9b801e3e
julioct99 <jcasallot01@gmail.com>
2021-09-19 11:40:19 +0200

    Episode 7

--- 02-face-part-2/src/App.js
+++ 02-face-part-2/src/App.js
@@ -1,3 +1,6 @@
+import { arc } from 'd3';
+
+// Face
 const width = 400;
 const height = 400;
 const centerX = width / 2;
@@ -5,23 +8,42 @@ const centerY = height / 2;
 const strokeWidth = 5;
 const r = centerY - strokeWidth / 2;
 
+// Eyes
 const eyeOffsetX = 90;
-const eyeOffsetY = 25;
+const eyeOffsetY = 75;
 const eyeRadius = 40;
+const innerEyeRadius = 35;
+const corneaRadius = 21;
+const pupilRadius = 11;
+const eyeColor = 'lightgreen';
+
+// Mouth
+const mouthWidth = 20;
+const mouthRadius = 125;
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2);
 
 function App() {
   return (
     <svg width={width} height={height}>
-      <circle
-        cx={centerX}
-        cy={centerY}
-        fill='yellow'
-        r={r}
-        stroke='black'
-        stroke-width={strokeWidth}
-      ></circle>
-      <circle cx={centerX - eyeOffsetX} cy={centerY - eyeOffsetY} r={eyeRadius} />
-      <circle cx={centerX + eyeOffsetX} cy={centerY - eyeOffsetY} r={eyeRadius} />
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        <circle fill='yellow' r={r} stroke='black' stroke-width={strokeWidth}></circle>
+
+        <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+        <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={innerEyeRadius} fill='white' />
+        <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={corneaRadius} fill={eyeColor} />
+        <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={pupilRadius} />
+
+        <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+        <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={innerEyeRadius} fill='white' />
+        <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={corneaRadius} fill={eyeColor} />
+        <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={pupilRadius} />
+
+        <path d={mouthArc()} />
+      </g>
     </svg>
   );
 }
---
+++ 02-face-part-2/src/Face.js
@@ -0,0 +1,5 @@
+const Face = ({ fill, radius, stroke, strokeWidth }) => {
+  return <circle fill='yellow' r={radius} stroke='black' stroke-width={strokeWidth} />;
+};
+
+export default Face;
